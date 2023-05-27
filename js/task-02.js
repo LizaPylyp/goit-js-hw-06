402923aa52ef303eee9fc35adc61e1af198b6dcb
Liza <lizkapylypchuk123456@gmail.com>
2023-05-27 20:36:45 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments"
 ];
+
+const ul = document.querySelector('#ingredients');
+console.log(`${ul}`);
+ingredients.forEach(ingredient => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  ul.appendChild(li);
+});
+
