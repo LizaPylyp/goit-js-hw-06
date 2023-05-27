f345d911b9fa4f9f80e57045c1c75673309eca9c
Liza <lizkapylypchuk123456@gmail.com>
2023-05-27 20:37:18 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,34 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+const controlsDiv = document.querySelector("#controls");
+  const input = controlsDiv.querySelector("input");
+  const createButton = controlsDiv.querySelector("[data-create]");
+  const destroyButton = controlsDiv.querySelector("[data-destroy]");
+  const boxesDiv = document.querySelector("#boxes");
+
+  createButton.addEventListener("click", createBoxes);
+  destroyButton.addEventListener("click", destroyBoxes);
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0")}`;
+  }
+
+  function createBoxes() {
+    const amount = Number(input.value);
+    const boxes = [];
+
+    for (let i = 0; i < amount; i++) {
+      const size = 30 + i * 10;
+      const box = document.createElement("div");
+      box.style.width = size + "px";
+      box.style.height = size + "px";
+      box.style.backgroundColor = getRandomHexColor();
+      boxes.push(box);
+    }
+
+    boxesDiv.append(...boxes);
+  }
+
+  function destroyBoxes() {
+    boxesDiv.innerHTML = "";
+  }
